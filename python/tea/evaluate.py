d97ea53f37034836258646c29a57e1a6ceb66a78
Eunice Jun <eunice.m.jun@gmail.com>
2019-01-07 11:04:54 -0800

    Refactored evaluation to take into consideration data types and between/within.

--- python/tea/evaluate.py
+++ python/tea/evaluate.py
@@ -7,20 +7,8 @@ import statsmodels.formula.api as smf
 import numpy as np # Use some stats from numpy instead
 
 # Eval : Dataset -> Model -> List[Hyps] -> ????
-
 # Eval : Model -> List[Hyps] -> (Dataset -> Result)
 
-# eval(x + x)(x -> 1)
-# def check_thing(ds):
-#     return Eval(model, hyps)(ds)
-
-# m1 = ????
-# m2 - ????
-
-# ds = ... 
-
-# for m in all_models:
-#     eval(ds, model, hyps)
 
 class Evaluator(object):
     def __init__(self, dataset, experiment_setup):
@@ -31,16 +19,61 @@ class Evaluator(object):
     def eval(self, model: Model):
         self.stats = {}
 
-        if (len(model.eq_independent_vars) == 1): # bivariate analysis
-            import pdb; pdb.set_trace()
-
+        dv = model.dependent_var
+        iv = model.eq_independent_vars
+        
+        if (isinstance(iv, Variable)): # only one independent variable --> BIVARIATE ANALYSIS
+            if (iv.dtype == DataType.ORDINAL or iv.dtype == DataType.NOMINAL): 
+                if (iv.etype == Experiment_DataType.BETWEEN_SUBJECTS): 
+                    if (dv.dtype == DataType.INTERVAL and isnormal(dv.data)): 
+                        # oneway interval ANOVA
+                        pass
+                    elif (dv.dtype == DataType.INTERVAL or dv.dtype == DataType.ORDINAL):
+                        # kruskal wallis 
+                        pass
+                    elif (dv.dtype == DataType.NOMINAL): 
+                        # chi square
+                        pass
+                    else: 
+                        raise ValueError(f"Do not support this yet: IV{iv.dtype}, {iv.etype} and DV{dv.dtype}, {dv.etype}")
+                elif (iv.etype == Experiment_DataType.WITHIN_SUBJECTS):
+                    if (dv.dtype == DataType.INTERVAL and isnormal(dv.data)): 
+                        # one way repeated measures ANOVA
+                        pass
+                    elif (dv.dtype == DataType.INTERVAL or dv.dtype == DataType.ORDINAL): 
+                        # Friedman test
+                        pass
+                    elif (dv.dtype == DataType.NOMINAL):
+                        if (len(dv.categories) == 2): 
+                            # repeated measures logistic regression
+                            pass
+                        else: 
+                            print('What do we do here?')
+            else: # (iv.dtype == DataType.INTERVAL or iv.dtype == DataType.RATIO): 
+                if (dv.dtype == DataType.INTERVAL and isnormal(dv.data)): 
+                    # correlation or simple linear regression
+                    pass
+                elif (dv.dtype == DataType.INTERVAL or dv.dtype == DataType.ORDINAL): 
+                    # non-parametric correlation
+                    pass
+                elif (dv.dtype == DataType.NOMINAL): 
+                    # simple logistic regression 
+                    pass
+                else: 
+                    raise ValueError(f"Do not support this yet: IV{iv.dtype}, {iv.etype} and DV{dv.dtype}, {dv.etype}")
 
         else: #multivariate analysis
-            eq = str(model.dependent_var) + '~' + str(model.eq_independent_vars)
-
-            m = smf.ols(formula=eq, data=self.dataset) # use patsy for R-style equations
-            results = m.fit()
-            self.stats[eq] = results._results.__dict__
+            # TODO: RATIO data? ORDINAL Data? 
+            if (dv.dtype == DataType.INTERVAL and isnormal(dv.data)):
+                # multiple regression
+                regression(self.dataset, model, self.stats)
+                # ANCOVA 
+                pass
+            elif (dv.dtype == DataType.NOMINAL): 
+                # multiple logistic regression or discriminant analysis
+                pass
+            else: 
+                raise ValueError(f"Do not support this yet: IV{iv.dtype}, {iv.etype} and DV{dv.dtype}, {dv.etype}")
 
 
 def evaluate(dataset: Dataset, experiment_setup: Experiment_SetUp, model: Model, hypothesis: Hypothesis):
@@ -48,3 +81,8 @@ def evaluate(dataset: Dataset, experiment_setup: Experiment_SetUp, model: Model,
     e.eval(model)
     return e.stats
 
+def regression(dataset, model: Model, stats): 
+    eq = str(model.dependent_var) + '~' + str(model.eq_independent_vars)
+    m = smf.ols(formula=eq, data=dataset) # use patsy for R-style equations
+    results = m.fit()
+    stats[eq] = results._results.__dict__
