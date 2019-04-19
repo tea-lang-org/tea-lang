02f3aa8f5fefb8619e8b62b00dd3ef963ce9b969
Eunice Jun <eunice.m.jun@gmail.com>
2019-04-18 22:22:41 -0700

    Start using setuptools

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(name='tea',
+      version='0.1',
+      description='Tea: A High-level Language and Runtime System to Automate Statistical Analysis',
+      url='https://github.com/emjun/tea-lang',
+      author='Eunice Jun',
+      author_email='emjun@cs.washington.edu',
+      license='MIT',
+      packages=find_packages()
+)
