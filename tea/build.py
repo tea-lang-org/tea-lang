0bbe24b5801c363b25f50fab37a8d9e71606cc30
Malin <szyszka45@gmail.com>
2019-10-15 19:58:06 +0200

    general cleanup and PEP8 compability

--- tea/build.py
+++ tea/build.py
@@ -1,10 +1,6 @@
 from tea.runtimeDataStructures.dataset import Dataset
-from tea.ast import (  Variable, DataType, Literal,
-                    Relate, Relationship
-                )
+from tea.ast import (Variable, DataType, Literal, Relate, Relationship)
 
-
-from typing import Dict, Union
 from collections import OrderedDict
 
 iv_identifier = 'independent variable'
@@ -18,19 +14,23 @@ categorical_comparators = ['>', '<', '==', '!=']
 continuous_positive_relationship = '+'
 continuous_negative_relationship = '-'
 
+
 def const(val):
     return Literal(val)
+
 
 def ordinal(var_name: str, ordered_categories: list):
     # Create order tuple
     categories = OrderedDict()
     for i, c in enumerate(ordered_categories):
-        categories[c] = i+1 # start at 1 not 0
-        
+        categories[c] = i + 1  # start at 1 not 0
+
     return Variable.from_spec(var_name, DataType.ORDINAL, categories, [1, len(categories)])
+
 
 def isordinal(var: Variable):
     return var.dtype is DataType.ORDINAL
+
 
 def nominal(var_name: str, unordered_categories: list):
     categories = OrderedDict()
@@ -38,49 +38,62 @@ def nominal(var_name: str, unordered_categories: list):
         categories[c] = -1
     return Variable.from_spec(var_name, DataType.NOMINAL, categories, None)
 
+
 def isnominal(var: Variable):
     return var.dtype is DataType.NOMINAL
 
-def interval(var_name: str, drange: list=None):
-    return Variable.from_spec(var_name, DataType.INTERVAL, None, drange) # treat range like categories, check that all values are within range
+
+def interval(var_name: str, drange: list = None):
+    return Variable.from_spec(var_name, DataType.INTERVAL, None,
+                              drange)  # treat range like categories, check that all values are within range
+
 
 def isinterval(var: Variable):
     return var.dtype is DataType.INTERVAL
 
-def ratio(var_name: str, drange: list=None):
-    return Variable.from_spec(var_name, DataType.RATIO, None, drange) # treat range like categories, check that all values are within range
+
+def ratio(var_name: str, drange: list = None):
+    return Variable.from_spec(var_name, DataType.RATIO, None,
+                              drange)  # treat range like categories, check that all values are within range
+
 
 def isratio(var: Variable):
     return var.dtype is DataType.RATIO
 
+
 def isnumeric(var: Variable):
-    return (isratio(var) or isinterval(var))
+    return isratio(var) or isinterval(var)
+
 
 def iscategorical(var: Variable):
-    return (isnominal(var) or isordinal(var))
+    return isnominal(var) or isordinal(var)
+
 
 # @param pid is the name of the column with participant ids
 def load_data(source_name: str, vars: list, pid: str):
     return Dataset(source_name, vars, pid)
 
+
 def load_data_from_url(url: str, name: str):
     return Dataset.load(url, name)
 
-def select(var: Variable, op: str, other: Literal): 
-    if (op == '=='):
+
+def select(var: Variable, op: str, other: Literal):
+    if op == '==':
         return var.subset_equals(other)
-    elif (op == '!='):
+    elif op == '!=':
         return var.subset_not_equals(other)
-    elif (op == '<'):
+    elif op == '<':
         return var.subset_lt(other)
-    elif (op == '<='):
+    elif op == '<=':
         return var.subset_le(other)
-    elif (op == '>'):
+    elif op == '>':
         return var.subset_gt(other)
-    elif (op == '>='):
+    elif op == '>=':
         return var.subset_ge(other)
-    else: 
+    else:
         raise ValueError(f"Do not support the operator{op}")
+
 
 # @param vars: list of all vars in tuple form (Variable, 'iv/dv/variable')
 # @param role: "role" of Variable (i.e., independent variable, dependent variable, variable)
@@ -90,7 +103,7 @@ def select(var: Variable, op: str, other: Literal):
 #     for v,r in vars:
 #         if r == role: 
 #             vars_role.append(v)
-    
+
 #     return vars_role
 
 # TODO: need to check that the prediction is well-formed (VALUES that are ordered exist, for example)
@@ -118,36 +131,38 @@ def select(var: Variable, op: str, other: Literal):
     --> in multivariate case, the types of factors don't have a bearing on the ....?
 """
 
+
 # def is_var_in_list(var_name: str, vars: list):
 #     names = [v.name for v in vars]
 
 #     return var_name in names
 
 def get_var_from_list(var_name: str, vars: list):
-    for v in vars: 
+    for v in vars:
         if v.name == var_name:
             return v
-    
-    return None # Does not exist
+
+    return None  # Does not exist
+
 
 def is_well_formed_prediction(prediction_type: str, vars: list, prediction: str):
     # import pdb; pdb.set_trace()
-    if (categorical_prediction == prediction_type):
+    if categorical_prediction == prediction_type:
         var_ind = prediction.index(categorical_prediction_delimiter)
         var_name = prediction[:var_ind].strip()
 
         # Does the variable actually exist in the list of variables?
         var = get_var_from_list(var_name, vars)
-        return var != None
+        return var is not None
         # if var:
         #     # Does the category exist in the var? (implictly checks that var is a categorical variable)
         #     return var.category_exists(category)
         # else: 
         #     return ValueError(f"The variable used in prediction{var} is not in list of variables comparing{vars}")
-    elif (continuous_prediction == prediction_type):
+    elif continuous_prediction == prediction_type:
 
-         # Prediction includes categorical variable value
-        if (categorical_prediction_delimiter in prediction): 
+        # Prediction includes categorical variable value
+        if categorical_prediction_delimiter in prediction:
             cat_delimiter_ind = prediction.index(categorical_prediction_delimiter)
             num_delimiter_ind = prediction.index(continuous_prediction_delimiter)
 
@@ -155,86 +170,87 @@ def is_well_formed_prediction(prediction_type: str, vars: list, prediction: str)
             # categorical_var = get_var_from_list(categorical_var_name, vars)
 
             # LHS is categorical
-            if (cat_delimiter_ind < num_delimiter_ind): 
+            if cat_delimiter_ind < num_delimiter_ind:
                 lhs = categorical_var_name.strip()
-                category = lhs[cat_delimiter_ind+1:num_delimiter_ind]
+                category = lhs[cat_delimiter_ind + 1:num_delimiter_ind]
                 lhs_var = get_var_from_list(lhs, vars)
-                assert(lhs_var.category_exists(category))
-                rhs = prediction[num_delimiter_ind+1:].strip()
-            else: 
-                assert(cat_delimiter_ind > num_delimiter_ind)
+                assert (lhs_var.category_exists(category))
+                rhs = prediction[num_delimiter_ind + 1:].strip()
+            else:
+                assert (cat_delimiter_ind > num_delimiter_ind)
                 lhs = prediction[:num_delimiter_ind].strip()
-                rhs = prediction[num_delimiter_ind+1:cat_delimiter_ind].strip()
-                category = rhs[cat_delimiter_ind+1:].strip()
+                rhs = prediction[num_delimiter_ind + 1:cat_delimiter_ind].strip()
+                category = rhs[cat_delimiter_ind + 1:].strip()
                 rhs_var = get_var_from_list(rhs, vars)
-                assert(rhs_var.category_exists(category))
-        
+                assert (rhs_var.category_exists(category))
+
         # Prediction does not include categorical variable value (can still have a categorical variable)
-        else: 
+        else:
             delimiter_ind = prediction.index(continuous_prediction_delimiter)
             lhs = prediction[:delimiter_ind].strip()
-            rhs = prediction[delimiter_ind+1:].strip()
+            rhs = prediction[delimiter_ind + 1:].strip()
 
-        if (continuous_negative_relationship in lhs):
-            lhs = lhs[lhs.index(continuous_negative_relationship)+1:].strip()
-        elif (continuous_positive_relationship in lhs): 
-            lhs = lhs[lhs.index(continuous_negative_relationship)+1:].strip()
-        else: 
+        if continuous_negative_relationship in lhs:
+            lhs = lhs[lhs.index(continuous_negative_relationship) + 1:].strip()
+        elif continuous_positive_relationship in lhs:
+            lhs = lhs[lhs.index(continuous_negative_relationship) + 1:].strip()
+        else:
             pass
-        
-        if (continuous_negative_relationship in rhs):
-            rhs = rhs[rhs.index(continuous_negative_relationship)+1:].strip()
-        elif (continuous_positive_relationship in rhs): 
-            rhs = rhs[rhs.index(continuous_positive_relationship)+1:].strip()
-        else: 
+
+        if continuous_negative_relationship in rhs:
+            rhs = rhs[rhs.index(continuous_negative_relationship) + 1:].strip()
+        elif continuous_positive_relationship in rhs:
+            rhs = rhs[rhs.index(continuous_positive_relationship) + 1:].strip()
+        else:
             pass
-        
+
         return get_var_from_list(lhs, vars) and get_var_from_list(rhs, vars)
-    else: 
+    else:
         raise ValueError(f"Malformed prediction. Is neither categorical nor numeric:{prediction}")
+
 
 def create_prediction(prediction_type: str, vars: list, prediction: str):
     global categorical_prediction, categorical_prediction_delimiter, categorical_comparators
     global continuous_prediction, continuous_prediction_delimiter, positive_relationship, negative_relationship
 
-    if (categorical_prediction == prediction_type): 
+    if categorical_prediction == prediction_type:
         # comparator = None
-        
-        for c in categorical_comparators: 
-            if c in prediction: 
+
+        for c in categorical_comparators:
+            if c in prediction:
                 delimiter_ind = prediction.index(categorical_prediction_delimiter)
                 comparator_ind = prediction.index(c)
 
                 var_name = prediction[:delimiter_ind].strip()
                 var = get_var_from_list(var_name, vars)
-                lhs = prediction[delimiter_ind+1:comparator_ind].strip()
+                lhs = prediction[delimiter_ind + 1:comparator_ind].strip()
                 # Add len(c) to index in case comparator is longer than one character
-                rhs = prediction[comparator_ind+len(c):].strip()
-                
+                rhs = prediction[comparator_ind + len(c):].strip()
+
                 types = set(type(k) for k in var.categories.keys())
-                assert(len(types) == 1) # Assert all keys have the same type
+                assert (len(types) == 1)  # Assert all keys have the same type
                 cat_type = next(iter(types))
 
-                if isinstance(lhs, str) and cat_type == int: 
+                if isinstance(lhs, str) and cat_type == int:
                     lhs = int(lhs)
                     rhs = int(rhs)
-                
-                assert(lhs in var.categories.keys())
-                assert(rhs in var.categories.keys())
-                if c == '<': 
+
+                assert (lhs in var.categories.keys())
+                assert (rhs in var.categories.keys())
+                if c == '<':
                     return [const(lhs) < const(rhs)]
-                elif c == '>': 
+                elif c == '>':
                     return [const(lhs) > const(rhs)]
-                elif c == '==': 
-                    return [const(lhs) ==  const(rhs)]
-                else: 
-                    assert(c == '!=')
+                elif c == '==':
+                    return [const(lhs) == const(rhs)]
+                else:
+                    assert (c == '!=')
                     return [const(lhs) != const(rhs)]
     else:
-        assert(continuous_prediction == prediction_type)
+        assert (continuous_prediction == prediction_type)
 
         # Prediction includes categorical variable value
-        if (categorical_prediction_delimiter in prediction): 
+        if categorical_prediction_delimiter in prediction:
             cat_delimiter_ind = prediction.index(categorical_prediction_delimiter)
             num_delimiter_ind = prediction.index(continuous_prediction_delimiter)
 
@@ -242,86 +258,88 @@ def create_prediction(prediction_type: str, vars: list, prediction: str):
             # categorical_var = get_var_from_list(categorical_var_name, vars)
 
             # LHS is categorical
-            if (cat_delimiter_ind < num_delimiter_ind): 
+            if cat_delimiter_ind < num_delimiter_ind:
                 lhs = categorical_var_name.strip()
-                rhs = prediction[num_delimiter_ind+1:].strip()
-            else: 
-                assert(cat_delimiter_ind > num_delimiter_ind)
+                rhs = prediction[num_delimiter_ind + 1:].strip()
+            else:
+                assert (cat_delimiter_ind > num_delimiter_ind)
                 lhs = prediction[:num_delimiter_ind].strip()
-                rhs = prediction[num_delimiter_ind+1:cat_delimiter_ind].strip()
-        
+                rhs = prediction[num_delimiter_ind + 1:cat_delimiter_ind].strip()
+
         # Prediction does not include categorical variable value (can still have a categorical variable)
-        else: 
+        else:
             delimiter_ind = prediction.index(continuous_prediction_delimiter)
             lhs = prediction[:delimiter_ind].strip()
-            rhs = prediction[delimiter_ind+1:].strip()
+            rhs = prediction[delimiter_ind + 1:].strip()
 
-            lhs_dir = '+' # Positive relationship/change in variable is the default
-            rhs_dir = '+' # Positive relationship/change in variable is the default
-            lhs_var = None
-            rhs_var = None
+            lhs_dir = '+'  # Positive relationship/change in variable is the default
+            rhs_dir = '+'  # Positive relationship/change in variable is the default
 
-            if (lhs.startswith(continuous_negative_relationship)): # "as LHS decreases,..."
+            if lhs.startswith(continuous_negative_relationship):  # "as LHS decreases,..."
                 lhs = lhs[1:]
                 lhs_dir = '-'
                 lhs_var = get_var_from_list(lhs, vars)
-            else: 
+            else:
                 # do nothing to lhs, lhs_dir
                 lhs_var = get_var_from_list(lhs, vars)
 
-            if rhs.startswith(continuous_negative_relationship): 
+            if rhs.startswith(continuous_negative_relationship):
                 rhs = rhs[1:]
                 rhs_dir = '-'
                 rhs_var = get_var_from_list(rhs, vars)
-            else: 
+            else:
                 # do nothing to rhs, rhs_dir
                 rhs_var = get_var_from_list(rhs, vars)
 
-            if lhs_dir == rhs_dir: # handles +lhs, +rhs and -lhs, 
+            if lhs_dir == rhs_dir:  # handles +lhs, +rhs and -lhs,
                 return Relationship(lhs_var).positive(Relationship(rhs_var))
-            else: # handles +lhs, -rhs and -lhs, +rhs
+            else:  # handles +lhs, -rhs and -lhs, +rhs
                 return Relationship(lhs_var).negative(Relationship(rhs_var))
 
- # def predict(factors: list, outcome: Variable, prediction: str=None):    
-def predict(vars: list, predictions: list=None):   
+
+# def predict(factors: list, outcome: Variable, prediction: str=None):
+def predict(vars: list, predictions: list = None):
     formulated_predictions = []
 
     # Validate well-formedness of predictions
-    if predictions: 
+    if predictions:
         # Check that each prediction is well-formed
-        for p in predictions: 
+        for p in predictions:
             assert (isinstance(p, str))
             # Does the prediction pertain to categorical data?
             if categorical_prediction_delimiter in p and continuous_prediction_delimiter not in p:
-                assert(is_well_formed_prediction(categorical_prediction, vars, p))
+                assert (is_well_formed_prediction(categorical_prediction, vars, p))
                 pred = create_prediction(categorical_prediction, vars, p)
-                            
+
             # Does the prediction pertain to numerical data?
-            elif continuous_prediction_delimiter in p: 
-                assert(is_well_formed_prediction(continuous_prediction, vars, p))
+            elif continuous_prediction_delimiter in p:
+                assert (is_well_formed_prediction(continuous_prediction, vars, p))
                 # import pdb; pdb.set_trace()
                 pred = create_prediction(continuous_prediction, vars, p)
-    
+
             # Prediction pertains to neither categorical nor numerical data                 
-            else: 
+            else:
                 raise ValueError(f"Prediction is malformed: {p}")
-            
+
             formulated_predictions.append(pred)
-    
+
     return formulated_predictions
+
 
 # Generic interface for observing relationships among variables (observational studies and experiments)
 # @params: vars should be a list of Variables
-def relate(vars: list, prediction: list=None) : 
+def relate(vars: list, prediction: list = None):
     return Relate(vars, predict(vars, prediction))
 
-# @params: iv could be the list of variables/groups want to compare on dv - may only want to compare 2 groups, not all conditions
+
+# @params: iv could be the list of variables/groups want to compare on dv -
+# may only want to compare 2 groups, not all conditions
 # def compare(var_1: Variable, var_2: Variable, prediction: str=None, when: str=None) :
 
 # def compare(iv: Variable, dv: Variable, prediction: str=None, when: str=None) :
-def compare(vars: list, prediction: list=None, when: str=None) :
+def compare(vars: list, prediction: list = None, when: str = None):
     return Relate(vars, predict(vars, prediction))
-    
+
     # if (isinstance(iv, Variable)):
     #     vars = [(iv, iv_identifier), (dv, dv_identifier)]
     #     if isnominal(iv) or isordinal(iv):
