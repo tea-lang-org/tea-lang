d13724938613e3acb3fe9da81878e2de5f9444a7
Eunice Jun <eunice.m.jun@gmail.com>
2019-11-19 17:28:15 -0800

    First pass at writing out a grammar. Need to write test case and test in tea_dsl before continue with development

---
+++ tea/tea_dsl.py
@@ -0,0 +1,17 @@
+from lark import Lark 
+
+tea_parser = Lark.open('tea_grammar.lark', parser='lalr')
+tea_p = tea_parser.parse
+
+def run_tea_program(program): 
+    global tea_parser
+    parse_tree = tea_parser.parse(program)
+    print(parse_tree)
+
+def test(): 
+    text = """
+        data data.csv
+    """
+
+if __name__ == '__main__': 
+    test()
