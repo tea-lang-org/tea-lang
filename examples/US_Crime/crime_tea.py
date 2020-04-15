1fec391c5fecee5011987dd635b6cee638170d6a
Melissa B <mbirch2@uw.edu>
2020-04-15 11:22:46 -0700

    updated crime_tea.py for git issue

--- examples/US_Crime/crime_tea.py
+++ examples/US_Crime/crime_tea.py
@@ -1,43 +1,41 @@
 import tea
 
-tea.data("./UScrime.csv")
+tea.data("https://homes.cs.washington.edu/~emjun/tea-lang/datasets/UScrime.csv")
 variables = [
     {
         'name' : 'So',
-        'data type' : 'nominal',   # Options: 'nominal', 'ordinal', 'interval', 'ratio'
+        'data type' : 'nominal',
         'categories' : ['0', '1']
     },
     {
         'name' : 'Prob',
-        'data type' : 'ratio',   # Options: 'nominal', 'ordinal', 'interval', 'ratio'
-        # 'range' : [0,1]   # optional
+        'data type' : 'ratio',
+        'range' : [0,1]   # optional
     },
     {
         'name' : 'Ineq',
-        'data type' : 'ordinal',   # Options: 'nominal', 'ordinal', 'interval', 'ratio'
-        'categories': [28,29,30]
-        # 'range' : [0,1]   # optional
+        'data type' : 'ratio'
     }
 ]
 
 tea.define_variables(variables)
 
 assumptions = {
-    'log normal distribution': [['Ineq']],
+    # 'log normal distribution': [['Ineq']],
     # 'equal variance': [['So', 'Ineq']],
     # 'groups normally distributed': [['So', 'Ineq']],
     'Type I (False Positive) Error Rate': 0.05,
 }
 
-tea.assume(assumptions, 'relaxed')
+tea.assume(assumptions)
 
 experimental_design = {
                         'study type': 'observational study',   # 'study type' could be 'experiment'
-                        'contributor variables': 'So',   # 'experiment's have 'independent variables'
+                        'contributor variables': 'Prob',   # 'experiment's have 'independent variables'
                         'outcome variables': 'Ineq',   # 'experiment's have 'dependent variables'
                     }
 tea.define_study_design(experimental_design)
 
 # tea.hypothesize(['So', 'Prob'], ['So:1 > 0'])
-tea.hypothesize(['So', 'Ineq'], ['So:1 > 0'])
-# tea.hypothesize(['Ineq', 'Prob'], ['Ineq ~ +Prob'])
+# tea.hypothesize(['So', 'Ineq'], ['So:1 > 0'])
+tea.hypothesize(['Ineq', 'Prob'], ['Ineq ~ +Prob'])
