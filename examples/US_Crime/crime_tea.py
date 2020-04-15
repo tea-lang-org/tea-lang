ee7e7ecfb743ee6e8e15c3c9508a14ee5192ff64
Melissa B <mbirch2@uw.edu>
2020-04-15 01:39:14 -0700

    adding to assumptions & hypothesis in docs

--- examples/US_Crime/crime_tea.py
+++ examples/US_Crime/crime_tea.py
@@ -10,24 +10,34 @@ variables = [
     {
         'name' : 'Prob',
         'data type' : 'ratio',   # Options: 'nominal', 'ordinal', 'interval', 'ratio'
-        'range' : [0,1]   # optional
+        # 'range' : [0,1]   # optional
+    },
+    {
+        'name' : 'Ineq',
+        'data type' : 'ordinal',   # Options: 'nominal', 'ordinal', 'interval', 'ratio'
+        'categories': [28,29,30]
+        # 'range' : [0,1]   # optional
     }
 ]
 
 tea.define_variables(variables)
 
 assumptions = {
-    'groups normally distributed': [['So', 'Prob']],
+    'log normal distribution': [['Ineq']],
+    # 'equal variance': [['So', 'Ineq']],
+    # 'groups normally distributed': [['So', 'Ineq']],
     'Type I (False Positive) Error Rate': 0.05,
 }
 
-tea.assume(assumptions)
+tea.assume(assumptions, 'relaxed')
 
 experimental_design = {
                         'study type': 'observational study',   # 'study type' could be 'experiment'
                         'contributor variables': 'So',   # 'experiment's have 'independent variables'
-                        'outcome variables': 'Prob',   # 'experiment's have 'dependent variables'
+                        'outcome variables': 'Ineq',   # 'experiment's have 'dependent variables'
                     }
 tea.define_study_design(experimental_design)
 
-tea.hypothesize(['So', 'Prob'], ['So:1 > 0'])
+# tea.hypothesize(['So', 'Prob'], ['So:1 > 0'])
+tea.hypothesize(['So', 'Ineq'], ['So:1 > 0'])
+# tea.hypothesize(['Ineq', 'Prob'], ['Ineq ~ +Prob'])
